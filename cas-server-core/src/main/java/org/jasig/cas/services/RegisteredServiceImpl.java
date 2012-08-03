84f97b8f8b96f271b12f35877e86049352afe3dc
Nathan Kopp <nathan.kopp@cru.org>
2012-08-03 16:25:56 -0400

    This includes two recommended enhancements to CAS: 1) an enhanced ServiceThemeResolver that can identify services by host header 2) additional algorithms and greater flexibility for registered service URL matching
    Note that these have not yet been tested against the 3.5.x codebase, but they should work properly.

--- cas-server-core/src/main/java/org/jasig/cas/services/RegisteredServiceImpl.java
+++ cas-server-core/src/main/java/org/jasig/cas/services/RegisteredServiceImpl.java
@@ -18,18 +18,45 @@
  */
 package org.jasig.cas.services;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.regex.Pattern;
+
 import org.jasig.cas.authentication.principal.Service;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.util.PathMatcher;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.Transient;
 
 /**
- * Mutable registered service that uses Ant path patterns for service matching.
+ * Mutable registered service that uses Various types of patterns for service matching.
+ * 
+ * The default, for backwards compatibility, is Ant-style matching.
+ * 
+ * To use another form of pattern matchibng, simply prefix the serviceId with one of
+ * the following:
+ * 
+ *   regex: match using a regular expression
+ *   domain: a single domain - any path or protocol will match
+ *   domainlist: a comma-separated list of domains
+ * 
+ * Note that the matching algorithms probably should be made pluggable.  However, I do
+ * not recommend using the inheritance model adopted for RegexRegisteredService, since
+ * that model doesn't allow much runtime flexibility.  Instead, only the algorithm itself
+ * should be pluggable, probably configured using a key/value map that ties prefix strings
+ * with java classes that implement a particular matching method.  Note, though, that we cached
+ * the Pattern object for the regex version here (to improve performance). That might
+ * be more difficult (but certainly not impossible) with pluggable algorithms.  Also note
+ * that it would be great for the algorithm identifier to be stored in a separate field.
+ * I'd recommend reusing the expression_type database column that is currently used for the
+ * inheritance descriminator.  However, there's currently nothing in the UI to easiy set
+ * or change that value, so I'm sticking with the prefix for now.
  * 
  * @author Scott Battaglia
  * @author Marvin S. Addison
+ * @author Nathan Kopp
  * @version $Revision$ $Date$
  * @since 3.1
  */
@@ -41,17 +68,59 @@ public class RegisteredServiceImpl extends AbstractRegisteredService {
     private static final long serialVersionUID = -5906102762271197627L;
 
     private static final PathMatcher PATH_MATCHER = new AntPathMatcher();
+    
+    @Transient private Pattern serviceIdPattern = null;
 
     public void setServiceId(final String id) {
         this.serviceId = id;
     }
 
     public boolean matches(final Service service) {
-        return service != null && PATH_MATCHER.match(serviceId.toLowerCase(), service.getId().toLowerCase());
+        if(service==null) return false;
+        if(this.serviceId==null) return false;
+        if(this.serviceId.startsWith("regex:"))
+        {
+            if(serviceIdPattern==null) serviceIdPattern = Pattern.compile(this.serviceId.substring(6));
+            return serviceIdPattern.matcher(service.getId().toLowerCase()).matches();
+        }
+        else if(this.serviceId.startsWith("domain:"))
+        {
+            String domain = this.serviceId.substring(7).toLowerCase();
+            return checkDomainMatch(service, domain);
+        }
+        else if(this.serviceId.startsWith("domainlist:"))
+        {
+            String domainList = this.serviceId.substring(11).toLowerCase();
+            for(String domain : domainList.split(","))
+            {
+                if (checkDomainMatch(service, domain)) return true;
+            }
+            return false;
+        }
+        else
+        {
+            return PATH_MATCHER.match(this.serviceId.toLowerCase(), service.getId().toLowerCase());
+        }
     }
+    
+    private boolean checkDomainMatch(final Service service, String domain)
+    {
+        try
+        {
+            domain = domain.trim();
+            if(domain.startsWith(".")) domain = domain.substring(1);
+            URL url = new URL(service.getId().toLowerCase());
+            return url.getHost().equals(domain) || url.getHost().endsWith("."+domain);
+        }
+        catch (MalformedURLException e)
+        {
+            return false;
+        }
+    }
+
+    
 
     protected AbstractRegisteredService newInstance() {
         return new RegisteredServiceImpl();
     }
 }
-
